22bf52e886c04ca952d6f27054bef38af7623dfa
minsik1014 <ms03zzang@naver.com>
2025-09-12 15:55:52 +0900

    week 02: README.md & variables v0.3

--- KotlinBasics/app/src/main/java/com/kotlinbasics/MainActivity.kt
+++ KotlinBasics/app/src/main/java/com/kotlinbasics/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.kotlinbasics
 
 import android.os.Bundle
+import android.text.BoringLayout
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -28,20 +29,42 @@ class MainActivity : ComponentActivity() {
             }
         }
         week02Variables()
+        week02Functionos()
     }
 }
 
 fun week02Variables(){
-    println("Week02 Variables")
+    /*println("Week02 Variables")
     val courseName = "Mobile Programing"
     //courseName = "IoT Programing"
     var week = 1
     week = 2
 
     println("Course: $courseName")
-    println("Week: $week")
+    println("Week: $week")*/
+
+    println("== Kotlin Variables ==")
+
+    val name = "Android"
+    //val name: String = "Android" 타입 지정도 가능
+    var version = 8.1
+    println("Hello $name $version")
+
+    val age: Int = 23
+    val height: Double = 181.4
+    val isStudent: Boolean = true
+
+    println("AGE: $age,height: $height, isStudent: $isStudent ")
+
 }
 
+fun week02Functionos(){
+    println("Week 02 Functions")
+
+    fun greet(name: String) = "Hello, $name!"
+
+    println(greet("Android Developer"))
+}
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
