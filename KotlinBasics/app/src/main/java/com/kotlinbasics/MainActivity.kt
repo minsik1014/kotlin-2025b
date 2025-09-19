9567939d82cb229c661716d61964ac061207d040
minsik1014 <ms03zzang@naver.com>
2025-09-19 15:41:53 +0900

    week03: classes v0.2

--- KotlinBasics/app/src/main/java/com/kotlinbasics/MainActivity.kt
+++ KotlinBasics/app/src/main/java/com/kotlinbasics/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.kotlinbasics
 
 import android.os.Bundle
-import android.text.BoringLayout
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -11,7 +10,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.kotlinbasics.ui.theme.KotlinBasicsTheme
@@ -30,100 +28,134 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-        //week02Variables()
-        //week02Functionos()
+//        week02Variables()
+//        week02Functions()
         week03Classes()
-        week03Collections()
-
+//        week03Collections()
     }
 }
 
-private fun week02Variables(){
-    /*println("Week02 Variables")
-    val courseName = "Mobile Programing"
-    //courseName = "IoT Programing"
-    var week = 1
-    week = 2
+private fun week03Collections(){
+    Log.d("KotlinWeek03", "== Kotlin Collections ==")
 
-    println("Course: $courseName")
-    println("Week: $week")*/
+    val fruits = listOf("apple", "banana", "orange")
+    val mutableFruits = mutableListOf("kiwi", "watermelon")
+//    fruits.add("kiwi")
+    mutableFruits.add("banana")
 
-    println("== Kotlin Variables ==")
+    Log.d("KotlinWeek03", "Fruits : $fruits")
+    Log.d("KotlinWeek03", "Mutable Fruits : $mutableFruits")
 
-    val name = "Android"
-    //val name: String = "Android" 타입 지정도 가능
-    var version = 8.1
-    println("Hello $name $version")
+    val scores = mapOf("Kim" to 97, "Park" to 100, "Lee" to 99)
+    Log.d("KotlinWeek03", "Scores : $scores")
 
-    val age: Int = 23
-    val height: Double = 181.4
-    val isStudent: Boolean = true
+    for(fruit in mutableFruits){
+        Log.d("KotlinWeek03", "Fruit : $fruit")
+    }
 
-    println("AGE: $age,height: $height, isStudent: $isStudent ")
-
+    scores.forEach{(name, score) -> Log.d("KotlinWeek03","$name scored $score")}
 }
 
-private fun week02Functionos(){
-    println("Week 02 Functions")
-/*
-    fun greet(name: String) = "Hello, $name!"
+private fun week03Classes(){
+    Log.d("KotlinWeek03","== Kotlin Classes ==")
 
-    println(greet("Android Developer"))
+    class Person(val name: String, var age: Int){
+        fun introduce(){
+            Log.d("KotlinWeek03","안녕하세요, $name ($age 세)입니다.")
+        }
+        fun birthday(){
+            age++
+            Log.d("KotlinWeek03","$name 의 생일! 이제 $age 세...")
+        }
+    }
+    val person1 = Person("홍길동", 31)
+    person1.introduce()
+    person1.birthday()
 
-    */
-    println("== Kotlin Functinos ==")
+    class Animal(var species: String){
+        var weight: Double = 0.0
+        constructor(species: String, weight: Double) : this(species){
+            this.weight = weight
+            Log.d("KotlinWeek03","$species 의 무게 :  이제 $weight kg")
+        }
+        fun makeSound(){
+            Log.d("KotlinWeek03","$species 가 소리를 냅니다")
+        }
+    }
+    val puppy = Animal("강아지", 6.5)
+    puppy.makeSound()
 
-    fun greet(name: String): String{
-        return "Hello $name"
+//    class Student{
+//        var name: String = ""
+//        var age: Int = 0
+//
+//        fun introduce(){
+//            println("Hi, I'm $name and I'm $age years old")
+//        }
+//    }
+//    val student = Student()
+//    student.name = "Choi"
+//    student.age = 22
+//    student.introduce()
+//
+//    data class Person(val name: String, val age: Int)
+//
+//    val person1 = Person("Lee", 24)
+//    val person2 = Person("Lee", 24)
+//
+//    println("Person1: $person1")
+//    println("Equal?: ${person1 == person2}")
+}
+
+private fun week02Functions(){
+    println("Week02 Functions")
+//
+//    fun greet(name: String) = "Hello, $name!"
+//
+//    println(greet("Android Developer"))
+
+    println("== Kotlin Functions ==")
+
+    fun greet(name: String): String {
+        return "Hello, $name"
     }
 
     fun add(a: Int, b: Int) = a + b
 
-    fun introduce(name: String, age: Int=19){
+
+    fun introduce(name: String, age: Int = 19){
         println("My name is $name and I'm $age years old")
     }
 
     println(greet("Kotlin"))
-    println("Sum: ${add(5, 6)}")
-    introduce("Sim")
-    introduce("Kim", 23)
+    println("Sum : ${add(5, -71)}")
+    introduce("Park")
+    introduce("Kim", 29)
 }
 
-private fun week03Classes(){
-    println("== Kotlin Classes ==")
+private fun week02Variables(){
+//    println("Week02 Variables")
+//
+//    val courseName = "Mobile Programming"
+//    //courseName = "IoT Programming"
+//    var week = 1
+//    week = 2
+//
+//    println("Course : $courseName")
+//    println("Week : $week")
 
-    class Student{
-        var name:String = ""
-        var age: Int=0
+    println("== Kotlin Variables ==")
 
-        fun introduce(){
-            println("Hi, I'm $name and I'm $age years old")
-        }
-    }
+//    val name: String = "Android"
+    val name = "Android"
+    var version = 8.1
+    println("Hello $name $version")
 
-    val student = Student()
-    student.name="심민식"
-    student.age=23
-    student.introduce()
+    val age: Int = 23
+    val height: Double = 177.7
+    val isStudent: Boolean = false
 
-    data class Person(val name:String, val age:Int) //data를 붙이면 값을 비교, data를 지우면 메모리주소를 비교
-
-    val person1 = Person("Lee", 24)
-    val person2 = Person("Lee", 24)
-
-    println("Person1: $person1")
-    println("Equal? : ${person1==person2}")
-}
-
-private fun week03Collections(){
-    Log.d("KotlinWeek03","== Kotlin Collections ==")
-
-    val fruits = listOf("apple", "banana", "orange")
-    Log.d("KotlinWeek03","Fruits: $fruits")
-
-    for(fruit in fruits){
-        Log.d("KotlinWeek03","Fruit: $fruit")
-    }
+    println("Age : $age, Height : $height, Student: $isStudent")
 }
 
 @Composable
